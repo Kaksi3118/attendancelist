84e91c294855b00dbf0244ff2f157c97cbcb9401
BigBatman03 <m.chawinski@gmail.com>
2024-11-06 12:22:24 +0100

    adding import and export functions

---
+++ zadanieGrupowe.py
@@ -0,0 +1,17 @@
+def importStudents(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        students = [line.strip() for line in file.readlines()] 
+    return students
+
+
+def Export(filename, attendanceList):
+    with open(filename, 'w', encoding='utf-8') as file:
+        for student in attendanceList:
+            file.write(f"{student['Name']}, {student['Presence']}\n")
+
+filename = r"C:\michal\test.txt"
+students = importStudents(filename)
+print(students)
+
+
+
