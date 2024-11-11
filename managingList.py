e2aa57dfe2cc1eb345deef77eeea7f27a5fcee8c
kleszczuch <Marek.kleszcz2004@gmail.com>
2024-11-11 22:35:49 +0100

    modifing files

--- managingList.py
+++ managingList.py
@@ -1,71 +1,50 @@
 import csv
 import os
+class managingListClass:
 
+    def __init__(self):
+            self.students = []
 
-def addStudent(students_list, name, surname, id):
-    student = {
-        "name": name,
-        "surname": surname,
-        "id": id
-    }
-    students_list.append(student)
-    print(f"dodano studenta: {name} {surname}, numer studenta: {id}")
+    def addStudent(self, name, surname, id, date, attendance):
+        student = {
+            "name:": name,
+            "surname:": surname,
+            "id:": id,
+            "date:": date,
+            "attendance:": attendance
 
+        }
+        self.students.append(student)
+        print(f"dodano studenta: {name} {surname}, numer studenta: {id}")
+    def checkIfCreated(self, filename):
+        try:
+            if os.path.isfile(filename):
+                print(f"plik {filename} istnieje")
+            else:
+                fieldnames = ["name:", "surname:", "id:", "date:", "attendance:"]
+                with open(filename, mode='w', newline='') as file:
+                    writer = csv.DictWriter(file, fieldnames=fieldnames)
+                    writer.writeheader()
+                print(f"Nie znaleziono bazy uczniów. \nPusty plik {filename} został utworzony.")
+        except Exception as e:
+            print(f"Błąd podczas tworzenia pliku: {e}")
 
-def saveToFile(students_list, fileName="students_Database.csv"):
-    fieldnames = ["name", "surname", "id"]
-    isCreated = os.path.isfile(fileName)
+    def saveToFile(self, filename):
+        fieldnames = ["name:", "surname:", "id:", "date:", "attendance:"]
+        with open(filename, mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writerows(self.students)
+        print(f"Saved students list to: {filename}")
 
-    with open(fileName, mode='a' if isCreated else 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        if not isCreated:
-            writer.writeheader()
-        writer.writerows(students_list)
-    print(f"Saved students list to: {fileName}")
+    def importFromFile(self, fileName):
+        if os.path.isfile(fileName):
+            with open(fileName, mode='r', newline='') as file:
+                reader = csv.DictReader(file)
+                for student in reader:
+                    self.students.append(student)
 
-
-def importFromFile(fileName="students_Database.csv"):
-    students_list = []
-    if os.path.isfile(fileName):
-        with open(fileName, mode='r', newline='') as file:
-            reader = csv.DictReader(file)
-            for student in reader:
-                students_list.append(student)
-
-        x = len(students_list)
-        print(f"wczytano {x} studentow z listy")
-    else:
-        print(f"Plik nie istinieje, baza studentow w pliku csv zosatala utworzona")
-
-    return students_list
-
-def main():
-    wantToContinue = True
-    fileName = "students_Database.csv"
-    students_list = importFromFile(fileName)
-    while wantToContinue:
-        f = input("Co chcesz zrobic?"
-                  "\n1 - dodac studenta"
-                  "\n2 - wypisac liste studentow"
-                  "\nreszta - zamknij program\n")
-        if f == '1':
-            name = input("Podaj imie: ")
-            surname = input("Podaj nazwisko: ")
-            id = input("podaj id studenta: ")
-            addStudent(students_list, name, surname, id)
-            saveToFile(students_list, fileName)
-        elif f == '2':
-            print("Lista studentow:")
-            for student in students_list:
-                print(student)
+            x = len(self.students)
+            print(f"wczytano {x} studentow z listy")
         else:
-            wantToContinue = False
-            print("Dowidzenia!")
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-
+            print(f"Plik nie istinieje")
+        return self.selfstudents
