ea9c0db28cbde230d131c0c36d2544513fbeff02
Kaksi3118 <maksymilian.jura3118@gmail.com>
2024-11-12 19:18:45 +0100

    fix:changed code to fit the main file

--- check_attendance.py
+++ check_attendance.py
@@ -1,93 +1,81 @@
-import csv
 from ctypes import c_char_p
 from datetime import datetime
 from managingList import managingListClass
+import os
+from import_export import ImportExportClass
+import csv
+from attendance_data import AttendanceDataClass
+from managingList import managingListClass
 
+class importingFunctions: #Dla szybkiego dostepu do wszystkich funkcji
+      importExport = ImportExportClass()
+      attednanceEditor = AttendanceDataClass(student_list = [])
+      managingList = managingListClass()
+use = importingFunctions()
+
+def saveAttendance(self, filename):
+    fieldnames = ["name:","surname:","id:","attendance:"]
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writerows(self.attendance_list)
+    print(f"Saved attendance list to: {filename}")
 
 
 class CheckAttendanceClass:
     def __init__(self):
-            self.students = []
-            self.attendance_data = {}
+        self.students = []
+        self.date_text = ""
+        self.attendance_list = []
 
-    def load_file(self, date, filename):
-        self.attendance_data
-        fieldnames = ["name:", "surname:", "id:", "date:", "attendance:"]
-        with open(filename, newline='', encoding='utf-8') as csvfile:  #sczyta po kolei kolejne linijki pliku
-            reader = csv.reader(csvfile)
-            students = [row[0] for row in reader]
-
-    def save_file(self, file_path):
-
-        with open(file_path, mode='w', newline='', encoding='utf-8') as csvfile: #zapisuje date i imie/nazwisko ucznia z attendance_data
-             writer = csv.writer(csvfile)
-             for date, present_students in self.attendance_data.items():
-                for student in present_students:
-                   writer.writerow([date, student])
-
-    def attendance(self, date):
-        self.attendance_data
-        if date not in self.attendance_data:
-            self.attendance_data[date] = []
-
-        print(f"Obecność dnia {date}")
-        for student in self.students:
-            status = input(f"Czy {student} jest obecny? (y/n)").strip().lower()
-            if status == "y":
-                self.attendance_data[date].append(student)
-                print("Student został oznaczony jako 'obecny'")
-            elif status == "n":
-                print("Student został oznaczony jako 'nieobecny'")
-                continue
-            else:
-                print("Niepoprawny input. Student został pominięty i oznaczony jako 'nieobecny'")
-
-        print("Obecność zapisana")
-
-    def edit_attendance(self, date):
-        if date in self.attendance_data: #jeżeli jest taka data - edytuj tą date
-            print(f"Edytuje obecność dnia {date}")
-            for student in self.students:
-                if student in self.attendance_data[date]:
-                    print(f"{student} był obecny")
-                else:
-                    print(f"{student} był nieobecny")
-
-                status = input("Czy chcesz zmienić status? (y/n)").strip().lower()
-                if status == "y":
-                    if student in self.attendance_data[date]:
-                        self.attendance_data[date].remove(student)
-                        print("Student został oznaczony jako 'nieobecny'")
-                    else:
-                        self.attendance_data[date].append(student)
-                        print("Student został oznaczony jako 'obecny'")
-            print("Obecność zaaktualizowana")
-        else:
-            print("Brak zapisanej obecności na ten dzień")
+    def addAttendance(self, name, surname, ids, attendance):
+        student_attendance = {
+            "name:": name,
+            "surname:": surname,
+            "id:": ids,
+            "attendance:": attendance
+        }
+        self.attendance_list.append(student_attendance)
+        print(f"Dodano obecność studenta: {name} {surname}, id studenta: {ids}")
 
     def editPresence(self, filename):
-        studentList = managingListClass.importFromFile(filename)
-
         while True:
+            self.students = use.managingList.importFromFile(filename)
             print("1.Obecność")
-            print("2.Edytowanie obecności")
-            print("3.Zapis obecności do pliku")
-            print("4.Wyjdź")
+            print("2.Zapis obecności do pliku")
+            print("3.Wróć")
             choice = input("")
             if choice == "1":
-                global date
-                date_str = input("Podaj datę (YYYY-MM-DD): ").strip()  # podaj date obecności
-                date = datetime.strptime(date_str, "%Y-%m-%d").date()  # zamienia datę z formatu string na date
-                self.attendance(date)
+                isDateGood = False
+                while isDateGood == False:
+                    isDateGood = True
+                    self.date_text = input("Podaj date obecności: (yyyy.mm.dd)")
+                    try:
+                        date = datetime.strptime(self.date_text, "%Y.%m.%d").date()
+                    except ValueError:
+                        isDateGood = False
+                        print("Podano bledna wartosc!")
+                for student in self.students:
+                    name = student["name:"]
+                    surname = student["surname:"]
+                    ids = student["id:"]
+                    print(f"Czy {student["name:"]} {student["surname:"]} był obecny?")
+                    isPresentOrNo = False
+                    while isPresentOrNo == False:
+                        attendance = input("y/n:")
+                        if attendance == "y":
+                            attendance = "Yes"
+                            isPresentOrNo = True
+                        elif attendance == "n":
+                            attendance = "No"
+                            isPresentOrNo = True
+                    self.addAttendance(name, surname, ids, attendance)
             elif choice == "2":
-                self.edit_attendance(date)
+                savefile = self.date_text
+                if savefile == "":
+                    print("Nie sprawdzono żadnej obecności")
+                else:
+                    saveAttendance(self, "Obecność_"+savefile+".csv")
             elif choice == "3":
-                #save_path = input("Zapisz plik pod nazwą(.txt lub .csv): ")
-                self.save_file(filename)
-                print("Obecność zapisana do pliku")
-            elif choice == "4":
-                break
+                os.system("python AttendanceList.py")
             else:
                 print("Zły wybór, spróbuj ponownie")
-
-
